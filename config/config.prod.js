22bfc3aac185c9fb49a0be73f035aa6302e12f67
mitianyi <kimmmiy@sina.cn>
2020-09-14 18:38:25 +0800

    init project

--- config/config.prod.js
+++ config/config.prod.js
@@ -9,26 +9,13 @@ const path = require('path');
  */
 module.exports = appInfo => {
   /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
+     * built-in config
+     * @type {Egg.EggAppConfig}
+     **/
   const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1599807210902_4670';
-
-  // add your middleware config here
-  config.middleware = [];
-
   config.webpack = {
-    webpackConfigList: [ require('../build/webpack.dev.config') ],
-  };
-
-  config.view = {
-    mapping: {
-      '.ejs': 'ejs',
-    },
-    defaultViewEngine: 'ejs',
+    webpackConfigList: [ require('../build/webpack.build.config') ],
   };
 
   config.static = {
